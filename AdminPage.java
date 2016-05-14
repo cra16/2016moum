431491d705a7dd6a1ea3801af4bb50d8af1df59d
LeeDongYoung <dyl159@naver.com>
2016-05-14 17:34:29 +0900

    db connect 성공

--- AdminPage.java
+++ AdminPage.java
@@ -1,19 +1,22 @@
 package oodp;
 
+// import for view
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+// import for iterator
 import java.util.ArrayList;
 import java.util.Iterator;
 
+// view when click menu meanagement button
 class AdminPage extends JPanel implements ActionListener
 {
     JButton makeMenu = new JButton("메뉴 추가");
     JButton deleteMenu = new JButton("메뉴 삭제");
-    JButton modifyMenu = new JButton("메뉴 변경");
 
     ArrayList<Menu> mMenuList = new ArrayList<Menu>();
     ArrayList<JLabel> mJLabelList = new ArrayList<JLabel>();
+    
     JTextField  text1 = new JTextField(10);
     JTextField  text2 = new JTextField(10);
     JTextField  text3 = new JTextField(10);
@@ -21,21 +24,19 @@ class AdminPage extends JPanel implements ActionListener
 
     AdminPage()
     {
+    	// connect db and insert info to mMenuList
         Menu_Management.initialMenu(mMenuList);
 
         setSize(400, 300);
-
  
         add(makeMenu);
         add(deleteMenu);
-        add(modifyMenu);
         add(text1);
         add(text2);
         add(text3);
    
         makeMenu.addActionListener(this);
         deleteMenu.addActionListener(this);
-        modifyMenu.addActionListener(this);
     }
 
     public void actionPerformed(ActionEvent e)
@@ -43,8 +44,7 @@ class AdminPage extends JPanel implements ActionListener
     	
     	MainBoard.board.remove(MainBoard.board_panel);
         MainBoard.board.repaint();
-        MainBoard.board.revalidate();
-        
+        MainBoard.board.revalidate();   
         MainBoard.board_panel=this;
         MainBoard.board_panel.setBackground(Color.white);
         MainBoard.board_panel.setBounds(150,40,650,560);
@@ -57,22 +57,25 @@ class AdminPage extends JPanel implements ActionListener
         Menu menu;
         int i;
         int result;
+        
         // 메뉴생성을 할 시
         if(obj == makeMenu){
         	mMenuList.add(Menu_Management.makeMenu(value1,value2,Integer.parseInt(value3)));
 			Iterator<Menu> iter = mMenuList.iterator();
 			i = 0;
+			// view에 보이는 정보 초기화
 			while (iter.hasNext()) {
 		        Menu string = (Menu)iter.next();
 		        mJLabelList.add(new JLabel(""));
                 add(mJLabelList.get(i++));
 		    }
-
+			
+			// view에 새로운 정보 나타내기
 			Iterator<Menu> iter2 = mMenuList.iterator();
 			i = 0;
 			while (iter2.hasNext()) {
 		         menu = (Menu)iter2.next();
-              mJLabelList.get(i++).setText(menu.menuSection+menu.menuList+menu.menuPrice+"\n");
+                 mJLabelList.get(i++).setText(menu.menuSection+menu.menuList+menu.menuPrice+"\n");
 
 		    }			
 			
@@ -85,10 +88,7 @@ class AdminPage extends JPanel implements ActionListener
             Menu delete = new Menu();
             delete.menuSection = value1;
             delete.menuList = value2;
-            if(value3 != null)
             delete.menuPrice = Integer.parseInt(value3);
-            System.out.println(delete.menuSection);
-            System.out.println("Sdfsdff"+value3);
             result = Menu_Management.deleteMenu(mMenuList ,delete);
 
 			Iterator<Menu> iter = mMenuList.iterator();
@@ -106,18 +106,14 @@ class AdminPage extends JPanel implements ActionListener
               mJLabelList.get(i++).setText(menu.menuSection+menu.menuList+menu.menuPrice+"\n");
 
 		    }	
-	           if(result == 0) {
-	                System.out.println("delete Menu Success");
-	                JOptionPane.showMessageDialog(this, "해당 내용이 없습니다",
-	                        "Error", JOptionPane.ERROR_MESSAGE);
-	            }
-			
+			if(result == 0) {
+			        System.out.println("delete Menu Success");JOptionPane.showMessageDialog(this, "해당 내용이 없습니다",
+			        "Error", JOptionPane.ERROR_MESSAGE);
+			}
 			
         }
 
-        else if(obj == modifyMenu){
-           System.out.println("Customer_Manage");
-        }
+
 
 //        else{
 //            System.out.println("enter the valid username and password");
--- Menu_Management.java
+++ Menu_Management.java
@@ -3,6 +3,7 @@ package oodp;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -16,10 +17,40 @@ class Menu_Management extends Menu{
  // 파라미터로 넘어온 정보를 menu에 넣고 return 한다.
  // menu list가 return 값을 받는다.
  public static Menu makeMenu(String section, String list, int price){
+	 // exception handling
+	 if (section==null || list==null || price == 0) throw new NullPointerException("please stop");
+	
      Menu menu = new Menu();
      menu.menuList = list;
      menu.menuPrice = price;
      menu.menuSection = section;
+
+     Connection conn = null;
+     Menu val = new Menu();
+     try
+     {
+    	 Class.forName("com.mysql.jdbc.Driver").newInstance();
+    	 conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "gksehdeo357");
+    	 if(!conn.isClosed()){
+    		 System.out.println("Successfully connected to MySQL server...");
+    		 PreparedStatement s;
+    		 s = conn.prepareStatement
+    				 ("INSERT INTO menu VALUES(?,?,?,?,?)");
+    		 s.setString(1, section);
+    		 s.setString(2, list);
+    		 s.setInt(3, price);
+    		 s.setInt(4, 0);
+    		 s.setInt(5, 100);
+    		 int count = s.executeUpdate();
+    		 s.close();
+    		 System.out.println(count+"rows were inserted");
+    	 } 
+	 } 
+	 catch(Exception exc)
+	 {	
+		 System.err.println("Exception: " + exc.getMessage());
+	 }
+	 
      return menu;
  }
 
@@ -36,16 +67,50 @@ class Menu_Management extends Menu{
 	 i = 0;
 	 while (iter.hasNext()) {
 		 menu = (Menu)iter.next();
-         if(menu.menuList.equals(delete.menuList)&&menu.menuSection.equals(delete.menuSection)&&menu.menuPrice == delete.menuPrice){
+         if(menu.menuList.equals(delete.menuList)&&menu.menuSection.equals(delete.menuSection)
+            &&menu.menuPrice == delete.menuPrice){
+        	 deleteFromDB(delete);
         	 mMenuList.remove(i);
     		 result = 1;
     		 break;
          }
          i++;
-     }   
- 
+     }
+	 System.out.println(delete.menuSection);
+	 
+
      return result;
  }
+ 
+ private static void deleteFromDB(Menu delete){
+     Connection conn = null;
+     try
+     {
+    	 Class.forName("com.mysql.jdbc.Driver").newInstance();
+    	 conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "gksehdeo357");
+    	 if(!conn.isClosed()){
+    		 System.out.println("Successfully connected to MySQL server...");
+    		 PreparedStatement s;
+    		
+    		 String query = "delete from menu where section = ? and list = ? and price = ?";
+    	     PreparedStatement preparedStmt = conn.prepareStatement(query);
+    	     preparedStmt.setString(1, delete.menuSection);
+    	     preparedStmt.setString(2, delete.menuList);
+    	     preparedStmt.setInt(3, delete.menuPrice);
+    	 
+    	     // execute the preparedstatement
+    	     preparedStmt.execute();
+    	       
+    	     conn.close();    		 
+    	 } 
+	 } 
+	 catch(Exception exc)
+	 {	
+		 System.err.println("Exception: " + exc.getMessage());
+	 }
+	 
+ }
+ 
  public void modifyMenu(){
 
  }
@@ -69,16 +134,13 @@ class Menu_Management extends Menu{
 					val.menuPrice = rs.getInt("price");
 					val.curNum = rs.getInt("cur_num");
 					val.soldNum =rs.getInt("sold_num");
-					mMenuList.add(new Menu(val));
-					
-				}
-			
-				
-					
-			}
-	     }
+					mMenuList.add(new Menu(val));	
+				} // while end					
+			} // if end
+	     }  // try end
 	     catch(Exception exc)
-	    {			System.err.println("Exception: " + exc.getMessage());
+	    {	
+	    	 System.err.println("Exception: " + exc.getMessage());
 	    }
  }
  
